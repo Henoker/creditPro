a4af021181dc868cd7357f5a08c41f7274da85da
Henoker <hennybany@gmail.com>
2023-08-01 22:03:27 +0300

    feat: configured custom user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomerUserChangeForm
+
+CustomUser = get_user_model()
 
 # Register your models here.
+class CusterUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomerUserChangeForm
+    model = CustomUser
+    list_display= [
+        "email",
+        "username",
+        "is_superuser",
+    ]
+
+admin.site.register(CustomUser, CusterUserAdmin)
