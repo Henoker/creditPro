23a2e066d6680cc42856d045d920ea5fb9ec46d6
Henoker <hennybany@gmail.com>
2023-08-18 21:22:51 +0300

    feat: extended abstract user model

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,22 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    MAKER = 'maker'
+    CHECKER = 'checker'
+    STAFF = 'staff'
+    USER_ROLES = (
+        (MAKER, 'Maker'),
+        (CHECKER, 'Checker'),
+        (STAFF, 'staff')
+    )
+    
+    role = models.CharField(max_length=10, choices=USER_ROLES, default=STAFF)
+
+    # Add any additional fields you might need for your users
+    # For example: phone_number, address, etc.
+
+    def is_maker(self):
+        return self.role == self.MAKER
+
+    def is_checker(self):
+        return self.role == self.CHECKER
