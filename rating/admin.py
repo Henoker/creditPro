c46e76d98372413292104ff952cbcf65d6e4f97c
Henoker <hennybany@gmail.com>
2023-08-26 21:45:43 +0300

    feat: loan models added

--- rating/admin.py
+++ rating/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FinancialRiskParameter, QualityOfFinancialStatements
+from .models import FinancialRiskParameter, QualityOfFinancialStatements, LoanType, Loan, CreditAnalyst, CreditDecision
 
 class FinancialRiskParameterAdmin(admin.ModelAdmin):
     list_display = ('sector', 'measurement', 'min_value', 'max_value', 'score')
@@ -19,4 +19,31 @@ class QualityOfFinancialStatementsAdmin(admin.ModelAdmin):
 
 admin.site.register(QualityOfFinancialStatements, QualityOfFinancialStatementsAdmin)
 
+admin.site.register(LoanType)
+
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('loan_id', 'amount', 'loan_type')
+    list_filter = ('loan_type',)
+    search_fields = ('loan_type',)
+    ordering = ('loan_type',)
+    list_per_page = 20
+
+admin.site.register(Loan, LoanAdmin)
+
+class CreditAnalystAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    list_per_page = 20
+
+admin.site.register(CreditAnalyst, CreditAnalystAdmin)
+
+class CreditDecisionAdmin(admin.ModelAdmin):
+    list_display = ('decision_id', 'loan_id', 'analyst', 'date_of_decision', 'decision')
+    list_filter = ('decision',)
+    search_fields = ('decision',)
+    ordering = ('decision',)
+    list_per_page = 20
+admin.site.register(CreditDecision, CreditDecisionAdmin)
 
--- rating/models.py
+++ rating/models.py
@@ -1,5 +1,7 @@
 
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 class FinancialRiskParameter(models.Model):
     SECTOR_CHOICES = [
         ('Agriculture', 'Agriculture'),
@@ -43,4 +45,61 @@ class QualityOfFinancialStatements(models.Model):
     description = models.TextField()
     
     def __str__(self):
-        return f"{self.loan_exposure} - Quality of Financial Statements"
+        return f"{self.loan_exposure} - Quality of Financial Statement"
+
+
+class LoanType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Create the Loans and Advances model
+class Loan(models.Model):
+    loan_id = models.CharField(max_length=20, unique=True)
+    amount = models.DecimalField(max_digits=13, decimal_places=2)
+    loan_type = models.ForeignKey(LoanType, on_delete=models.CASCADE)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    term = models.PositiveIntegerField(help_text="Term in months")
+    disbursement_date = models.DateField()
+    maturity_date = models.DateField()
+    collateral = models.TextField(blank=True, null=True)
+    collateral_amount = models.DecimalField(max_digits=13, decimal_places=2)
+
+    # Add any additional fields as needed
+
+    def __str__(self):
+        return f"Loan ID: {self.loan_id}, Type: {self.loan_type}"
+    
+class CreditAnalyst(models.Model):
+    analyst_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100)
+    contact_information = models.CharField(max_length=200)
+    department = models.CharField(max_length=100)
+    experience_level = models.CharField(max_length=50)
+    
+
+    # Add any additional fields as needed
+
+    def __str__(self):
+        return f"Analyst ID: {self.analyst_id}, Name: {self.name}"
+
+# Create the Credit Decisions model
+class CreditDecision(models.Model):
+    DECISION_CHOICES = (
+        ('Approved', 'Approved'),
+        ('Denied', 'Denied'),
+        ('Pending', 'Pending'),
+    )
+
+    decision_id = models.CharField(max_length=20, unique=True)
+    loan_id = models.CharField(max_length=20)
+    analyst = models.ForeignKey(CreditAnalyst, on_delete=models.CASCADE)
+    date_of_decision = models.DateField()
+    decision = models.CharField(max_length=10, choices=DECISION_CHOICES, default='Pending')
+    comments = models.TextField(blank=True, null=True)
+
+    # Add any additional fields as needed
+
+    def __str__(self):
+        return f"Decision ID: {self.decision_id}, Loan ID: {self.loan_id}, Analyst: {self.analyst.name}, Decision: {self.get_decision_display()}"
